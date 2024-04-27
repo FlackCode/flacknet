6c0f2426aefb7032f8b63d130ea2ee8f73f402f6
flack <163582560+FlackCode@users.noreply.github.com>
2024-04-27 09:13:00 +0200

    Fixed responsiveness bug on smaller screens.
    Width wasnt being displayed correctly,

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ export default function About() {
         <div className="hero min-h-screen bg-base-100 border-b-2">
         <div className="hero-content flex-col">
             <h1 className="text-5xl font-bold underline">About me</h1>
-            <div className='hero-content flex-col lg:flex-row xl:max-w-5xl xsm:max-w-sm gap-16'>
-                <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
+            <div className='hero-content flex-col lg:flex-row md:max-w-5xl xsm:max-w-sm gap-16'>
+                <img src={image} className="md:max-w-sm rounded-lg shadow-2xl" />
                 <div>
                   <h1 className="text-5xl font-bold xl:text-left xsm:text-center">Hi, I'm Dmitriy, aka Flack!</h1>
                   <p className="py-6 xl:text-left xsm:text-center">Welcome to my corner of the web. I'm a frontend web developer with a passion for creating sleek and user-friendly applications. Over the last three years, I've honed my skills through dedicated IT courses, making me a knowledgeable PC user and a quick adapter to emerging technologies.</p>
