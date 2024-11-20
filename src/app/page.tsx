4a6d9ef90e9641b498b9b03fcec30994bc91257f
FlackCode <fl4ckc0de@gmail.com>
2024-11-20 20:01:18 +0200

    Added home and about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,51 @@
+import Image from "next/image";
 import Nav from "~/components/Nav";
 
 export default async function Home() {
-
   return (
-      <main className="bg-black text-slate-100">
-        <Nav />
-      </main>
+    <main className="bg-[#00000a] text-slate-100 flex flex-col min-h-screen">
+      <Nav />
+      {/* Home */}
+      <div className="flex flex-grow items-center justify-center min-h-screen">
+        <div className="text-center max-w-2xl px-4">
+          <p className="font-semibold text-lg text-slate-400">Hi, my name is</p>
+          <h1 className="text-6xl font-bold text-white">Flack.</h1>
+          <h2 className="text-5xl font-bold text-slate-200 mt-2">
+            Welcome to my website!
+          </h2>
+          <p className="text-slate-400 text-lg mt-8 leading-relaxed">
+            Hello👋, I am Dmitriy, aka Flack, and I&apos;m a{" "}
+            <span className="text-white font-medium">Front-end Web Developer</span>.
+            I build things for the web to provide the best{" "}
+            <span className="text-white font-medium">User Experience</span> possible.
+            <br />
+            I am a <span className="text-white font-medium">fast learner</span>, always
+            looking to improve and learn new technologies.
+          </p>
+        </div>
+      </div>
+
+      {/* About */}
+      <section className="h-screen flex items-center justify-center bg-[#00000a] px-8">
+        <div className="max-w-4xl flex flex-col md:flex-row gap-12 items-center">
+          <Image src={'/image.jpg'} alt="Profile Image" width={256} height={256} className="rounded-lg" />
+          <div className="text-slate-100">
+            <h2 className="text-4xl font-bold mb-4">About Me</h2>
+            <p className="text-lg leading-relaxed">
+              I am a 17-year-old programmer and front-end web developer. My primary focus is to create responsive, user-friendly websites
+              that follow modern technologies and provide the best experience possible.
+            </p>
+            <p className="text-lg leading-relaxed mt-4">
+              My other interests include math, game development, and playing the guitar. While web development fuels
+              my professional journey, I&apos;m also excited about branching into new realms.
+            </p>
+            <p className="text-lg leading-relaxed mt-4">
+              I&apos;m diving into game development and exploring the exciting world of 3d modelling in Blender. I am
+              currently learning languages like C++, Python, and Dart to create different cool and useful applications.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 export default function Nav () {
     return (
-        <div className="w-full flex tracking-wide font-semibold px-16 py-4">
+        <div className="bg-[#00000a] w-full flex tracking-wide font-semibold px-16 py-4 border-b border-slate-200 fixed z-10">
             <div className="flex items-end gap-6">
                 <h1 className="text-xl">flack.net</h1>
                 <nav className="flex gap-6">
